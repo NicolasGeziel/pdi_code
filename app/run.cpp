733e4912197fc9e6a1b46fcad0dc4c05137d1fde
Nicolas <nicolasgesielfg@gmail.com>
2025-09-02 12:35:07 -0300

    imagens

--- app/run.cpp
+++ app/run.cpp
@@ -10,6 +10,8 @@ int main() {
     cv::Mat img = cv::imread("../data/blobs-image.jpg");
     cv::Mat img2 = cv::imread("../data/ave-01.jpeg");
     cv::Mat img3 = cv::imread("../data/ave-02.jpeg");
+    cv::Mat img4 = cv::imread("../data/estrada1.png");
+    cv::Mat img5 = cv::imread("../data/estrada.png");
 
     if (img.empty() || img2.empty() || img3.empty()) {
         std::cerr << "Erro ao carregar a imagem\n";
@@ -18,8 +20,8 @@ int main() {
 
     cv::Mat grayscale = GrayscaleConverter::toGrayscaleUsingWeightedMean(img);
     cv::Mat grayscale2 = GrayscaleConverter::toGrayscaleUsingArithmeticMean(img);
-    cv::Mat addImages = ArithmeticOperations::addImages(img2, img3);
-    cv::Mat subtractImages = ArithmeticOperations::subtractImages(img2, img3);
+    cv::Mat addImages = ArithmeticOperations::addImages(img4, img5);
+    cv::Mat subtractImages = ArithmeticOperations::subtractImages(img4, img5);
     cv::Mat multiplyImages = ArithmeticOperations::multiplyImages(img2, img3);
     cv::Mat divideImages = ArithmeticOperations::divideImages(img2, img3);
     cv::Mat addScalar = ArithmeticOperations::addScalar(img, 50);
@@ -34,13 +36,13 @@ int main() {
     cv::Mat histogramColor = Histogram::viewColorHistogram(img);
     cv::Mat negative = Negative::applyNegative(img);
     
-
     cv::imshow("Original", img);
     cv::imshow("Grayscale Weighted", grayscale);
     cv::imshow("Grayscale Arithmetic", grayscale2);
     cv::waitKey(0);
     cv::imshow("Add Images", addImages);
     cv::imshow("Subtract Images", subtractImages);
+    cv::waitKey(0);
     cv::imshow("Multiply Images", multiplyImages);
     cv::imshow("Divide Images", divideImages);
     cv::waitKey(0);
@@ -61,6 +63,5 @@ int main() {
     cv::imshow("Negative", negative);
     cv::waitKey(0);
     cv::destroyAllWindows();
-
     return 0;
 }
